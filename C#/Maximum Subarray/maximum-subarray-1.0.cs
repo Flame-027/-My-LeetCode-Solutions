67b5516e4be7487a97b297a08586a347d3fe91d6
Husnain Majeed <husnainm3@gmail.com>
2022-12-14 17:15:05 +0000

    added my Max Subarray solution

---
+++ C#/Maximum Subarray/maximum-subarray-1.0.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    class program
+    {
+        public int MaxSubArray(int[] nums)
+        {
+
+          int maybeMax = nums[0];
+          int max= nums[0];
+
+          for (var i = 1; i <= nums.Length - 1; i++)
+          {
+           maybeMax += nums[i];
+           maybeMax = Math.Max(maybeMax, nums[i]);
+           max = Math.Max(maybeMax, max);
+          }
+          return max;
+        } 
+    }
+
+    // Constraints : input will always be a single int[] , output must be int[2] and contain the largest sum , solution must be < O(n)
+
+    // step 1 : loop through array and start adding elements to total
+                // step 1 : during loop if current index is larger than maybeMax - total, update maybeMax to equal current index
+                // step 2 : if maybeMax is larger than Max then Max equals to total
+}

6b4cdc0723ad73ec5b92d9f1c8e289f7872e7a09
Husnain Majeed <husnainm3@gmail.com>
2022-12-14 18:33:59 +0000

    Added my Contains Duplicate solution

---
+++ C#/Contains Duplicate/contains-duplicate-1.0.cs
@@ -0,0 +1,34 @@
+﻿// Runtime beats 99% of leetcode submissions.
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) 
+	{
+        if (nums == null || nums.Length <= 1) return false;
+
+        Dictionary<int, int> hashtable = new();
+        int i = 0;
+        int j = nums.Length - 1;
+
+        while(true)
+        { 
+            if (hashtable.ContainsKey(nums[i]))
+                return true;
+
+            if (i == j)
+                return false;
+
+            hashtable.TryAdd(nums[i], i);
+
+            if (hashtable.ContainsKey(nums[j]))
+                return true;
+
+            hashtable.TryAdd(nums[j], j);
+
+            if (j == i + 1)
+                return false;
+
+            i++;
+            j--;
+        }
+    }	
+}
